803dc03f75ee8d31e63d3dbc638e09fdca98e44e
GunpowderGuy <diegorosario2013@gmail.com>
2025-09-12 22:25:43 -0500

    Custom GHC.Generic implementation attempt

--- myproject/src/Main.hs
+++ myproject/src/Main.hs
@@ -4,10 +4,29 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+--for manual generic implementation
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+
+
+
 module Main where
 
-import Data.Aeson (FromJSON(..), Value)
+import Data.Aeson (FromJSON(..), Value, genericParseJSON, defaultOptions)
+-- last one for json instance based on manual Generic
+
 import GHC.Generics (Generic)
+
+
+import GHC.Generics
+  ( Generic(..), (:*:)(..), (:+:)(..)
+  , M1(..), K1(..), C1, D1, S1, Rec0
+  , Meta(..)                 -- 'MetaData, 'MetaCons, 'MetaSel
+  , FixityI(..)              -- PrefixI, InfixI ...
+  , SourceUnpackedness(..)   -- NoSourceUnpackedness
+  , SourceStrictness(..)     -- NoSourceStrictness
+  , DecidedStrictness(..)    -- DecidedLazy
+  )
 
 import GHC.Cmm.Dataflow.Label
 
@@ -22,10 +41,16 @@ import GHC.Cmm.Reg    (GlobalReg)      -- register type used by CmmProc
 import GHC.Cmm.Dataflow.Graph -- curent import
 import GHC.Cmm.Dataflow.Block
 
+--for manual generic implementation
+import GHC.Types.CostCentre (CostCentreStack)
+
+
 
 -- Allow Aeson Generic-based instance at the top level
 deriving instance Generic (GenCmmDecl CmmStatics CmmTopInfo CmmGraph)
-instance FromJSON (GenCmmDecl CmmStatics CmmTopInfo CmmGraph)
+--instance FromJSON (GenCmmDecl CmmStatics CmmTopInfo CmmGraph)
+instance FromJSON (GenCmmDecl CmmStatics CmmTopInfo CmmGraph) where
+  parseJSON _ = fail "dummy FromJSON for CmmNode"
 -- ^ Do NOT give a body here; you want the automatic (Generic) instance.
 
 -- ---- Dummy leaf instances to satisfy the Generic traversal ----
@@ -99,6 +124,67 @@ instance FromJSON SectionType where
 
 
 
+-- No-selector meta for S1 (use the type-level 'MetaSel constructor)
+type NoSel =
+  'MetaSel 'Nothing 'NoSourceUnpackedness 'NoSourceStrictness 'DecidedLazy
+
+
+type Rep_GenCmmStaticsTrue =
+  D1 ('MetaData "GenCmmStatics 'True" "GHC.Cmm" "ghc" 'False)
+    ( C1 ('MetaCons "CmmStaticsRaw" 'PrefixI 'False)
+        ( S1 NoSel (Rec0 CLabel)
+     :*: S1 NoSel (Rec0 [CmmStatic]) ) )
+
+
+instance Generic (GenCmmStatics 'True) where
+  type Rep (GenCmmStatics 'True) = Rep_GenCmmStaticsTrue
+  from (CmmStaticsRaw lbl statics) =
+    M1 (M1 ( M1 (K1 lbl) :*: M1 (K1 statics) ))
+  to (M1 (M1 (a :*: b))) =
+    CmmStaticsRaw (unK1 (unM1 a)) (unK1 (unM1 b))
+
+
+--------------------------------------------------------------------------------
+-- GenCmmStatics 'False  ==> CmmStatics  :+:  CmmStaticsRaw
+
+type Rep_GenCmmStaticsFalse =
+  D1 ('MetaData "GenCmmStatics 'False" "GHC.Cmm" "ghc" 'False)
+    ( -- CmmStatics :: CLabel -> CmmInfoTable -> CostCentreStack
+      --           -> [CmmLit] -> [CmmLit] -> GenCmmStatics 'False
+      C1 ('MetaCons "CmmStatics" 'PrefixI 'False)
+        ( S1 NoSel (Rec0 CLabel)
+     :*: S1 NoSel (Rec0 CmmInfoTable)
+     :*: S1 NoSel (Rec0 CostCentreStack)
+     :*: S1 NoSel (Rec0 [CmmLit])
+     :*: S1 NoSel (Rec0 [CmmLit]) )
+      :+:
+      -- CmmStaticsRaw :: CLabel -> [CmmStatic] -> GenCmmStatics a
+      C1 ('MetaCons "CmmStaticsRaw" 'PrefixI 'False)
+        ( S1 NoSel (Rec0 CLabel)
+     :*: S1 NoSel (Rec0 [CmmStatic]) )
+    )    
+
+instance Generic (GenCmmStatics 'False) where
+  type Rep (GenCmmStatics 'False) = Rep_GenCmmStaticsFalse
+  from (CmmStatics lbl info ccs lits refs) =
+    M1 (L1 (M1 ( M1 (K1 lbl)
+               :*: M1 (K1 info)
+               :*: M1 (K1 ccs)
+               :*: M1 (K1 lits)
+               :*: M1 (K1 refs) )))
+  from (CmmStaticsRaw lbl statics) =
+    M1 (R1 (M1 ( M1 (K1 lbl) :*: M1 (K1 statics) )))
+  to (M1 (L1 (M1 (a :*: b :*: c :*: d :*: e)))) =
+    CmmStatics (unK1 (unM1 a))
+               (unK1 (unM1 b))
+               (unK1 (unM1 c))
+               (unK1 (unM1 d))
+               (unK1 (unM1 e))
+  to (M1 (R1 (M1 (a :*: b)))) =
+    CmmStaticsRaw (unK1 (unM1 a)) (unK1 (unM1 b))
+
+--instance FromJSON (GenCmmStatics 'True) where
+--  parseJSON = genericParseJSON defaultOptions
+
 main :: IO ()
 main = putStrLn "Hello, World!"
-
